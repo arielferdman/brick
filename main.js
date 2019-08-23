bced6c007b6f6997344486aa167ab0b5d905d1c4
arielferdman <ariel.f@gurushots.com>
2019-08-23 03:24:11 +0300

    Second commit

--- main.js
+++ main.js
@@ -4,12 +4,63 @@ let player = {
     x: 30,
     y: 700,
     w: 140,
-    h: 40
+    h: 30
 }
+
+let KeyboardKeyCodes = {
+    left: 37,
+    up: 38,
+    right: 39,
+    down: 40
+}
+
+let currentKeyPress = '';
 
 let playerDrawArguments = [player.x, player.y, player.w, player.h];
 
 window.onload = () => {
         canvas = document.getElementById('GameBoard');
+        canvas.width = 800;
+        canvas.height = 800;
         ctx = canvas.getContext('2d');
+
+        frame();
 };
+
+function frame() {
+
+    update();
+    draw();
+
+    window.requestAnimationFrame(frame);
+}
+
+function update() {
+    switch (currentKeyPress) {
+        case KeyboardKeyCodes.left:
+            move(KeyboardKeyCodes.left);
+            break;
+        case KeyboardKeyCodes.right:
+            move(KeyboardKeyCodes.right);
+            break;
+    }
+}
+
+function draw(color) {
+
+}
+
+function drawPlayer(color) {
+    ctx.style = color;
+    ctx.rect(...playerDrawArguments);
+    ctx.stroke();
+}
+
+function move() {
+    ctx.style = 'white';
+    ctx.rect(...playerDrawArguments);
+    ctx.stroke
+}
+
+document.addEventListener('keypress', (event) => {currentKeyPress = event.code})
+document.addEventListener('keyup', () => {currentKeyPress = ''})
