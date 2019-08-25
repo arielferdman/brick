4a74f880e29a4a85c36699ccdb06682efa2d221d
arielferdman <ariel.f@gurushots.com>
2019-08-25 22:42:05 +0300

    player movement working

--- main.js
+++ main.js
@@ -1,26 +1,27 @@
 let canvas,ctx;
 
-let player = {
-    x: 30,
-    y: 700,
-    w: 140,
-    h: 30
-}
-
 let KeyboardKeyCodes = {
-    left: 37,
-    up: 38,
-    right: 39,
-    down: 40
+    left: 'Numpad4',
+    up: 'Numpad8',
+    right: 'Numpad6',
+    down: 'Numpad2'
 }
 
 let currentKeyPress = '';
 
-let playerDrawArguments = [player.x, player.y, player.w, player.h];
+let playerDrawArguments = [30, 700, 140, 30];
+let playerDrawArgsNames = {
+    x: 0,
+    y: 1,
+    w: 2,
+    h: 3
+}
+
+let playerMoveGap = 10;
 
 window.onload = () => {
         canvas = document.getElementById('GameBoard');
-        canvas.width = 800;
+        canvas.width = 801;
         canvas.height = 800;
         ctx = canvas.getContext('2d');
 
@@ -28,39 +29,78 @@ window.onload = () => {
 };
 
 function frame() {
-
     update();
     draw();
 
     window.requestAnimationFrame(frame);
 }
-
 function update() {
     switch (currentKeyPress) {
         case KeyboardKeyCodes.left:
             move(KeyboardKeyCodes.left);
+            currentKeyPress = '';
             break;
         case KeyboardKeyCodes.right:
             move(KeyboardKeyCodes.right);
+            currentKeyPress = '';
             break;
     }
 }
 
-function draw(color) {
-
+function draw() {
+    clearCanvas();
+    drawPlayer('black', playerDrawArguments);
 }
 
-function drawPlayer(color) {
-    ctx.style = color;
-    ctx.rect(...playerDrawArguments);
+function clearCanvas() {
+    ctx.clearRect(0,0, canvas.width, canvas.height)
+}
+
+function drawPlayer(color, drawArguments) {
+    ctx.beginPath();
+    ctx.strokeStyle = color;
+    ctx.rect(...drawArguments);
     ctx.stroke();
 }
 
-function move() {
-    ctx.style = 'white';
-    ctx.rect(...playerDrawArguments);
-    ctx.stroke
+function move(direction) {
+    switch (direction) {
+        case KeyboardKeyCodes.left:
+            if (playerDrawArguments[playerDrawArgsNames.x] >= canvas.offsetLeft)
+                playerDrawArguments[playerDrawArgsNames.x] -= playerMoveGap;
+            break;
+        case KeyboardKeyCodes.right:
+            if (playerDrawArguments[playerDrawArgsNames.x] +
+                playerDrawArguments[playerDrawArgsNames.w] +
+                playerMoveGap <= canvas.offsetWidth)
+                    playerDrawArguments[playerDrawArgsNames.x] += playerMoveGap;
+            break;
+    }
 }
 
-document.addEventListener('keypress', (event) => {currentKeyPress = event.code})
-document.addEventListener('keyup', () => {currentKeyPress = ''})
+function keypress(event) {
+    currentKeyPress = event.code;
+}
+
+function keyRelease() {
+    currentKeyPress = '';
+}
+
+document.addEventListener('keypress', keypress);
+document.addEventListener('keyup', keyRelease);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
